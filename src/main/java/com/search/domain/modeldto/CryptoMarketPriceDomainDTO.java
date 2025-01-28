d01e4884f8e09ac1c18b825f90455bd1bcdb90b0
damodar88 <damodarstt@gmail.com>
2025-01-28 18:17:29 -0300

    feat: Add builder domain

--- src/main/java/com/search/domain/modeldto/CryptoMarketPriceDomainDTO.java
+++ src/main/java/com/search/domain/modeldto/CryptoMarketPriceDomainDTO.java
@@ -8,7 +8,48 @@ public class CryptoMarketPriceDomainDTO {
     private String currency;
     private List<MarketPriceDomainDTO> marketPrices;
 
+    private CryptoMarketPriceDomainDTO(Builder builder){
+        this.cryptoId = builder.cryptoId;
+        this.currency = builder.currency;
+        this.marketPrices = builder.marketPrices;
+    }
 
+    public String getCryptoId() {
+        return cryptoId;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public List<MarketPriceDomainDTO> getMarketPrices() {
+        return marketPrices;
+    }
+
+    public static class Builder{
+        private String cryptoId;
+        private String currency;
+        private List<MarketPriceDomainDTO> marketPrices;
+
+        public Builder withCryptoId(String cryptoId) {
+            this.cryptoId = cryptoId;
+            return this;
+        }
+
+        public Builder withCurrency(String currency) {
+            this.currency = currency;
+            return this;
+        }
+
+        public Builder withMarketPrices(List<MarketPriceDomainDTO> marketPrices) {
+            this.marketPrices = marketPrices;
+            return this;
+        }
+
+        public CryptoMarketPriceDomainDTO build(){
+            return new CryptoMarketPriceDomainDTO(this);
+        }
+    }
 
 
 }
