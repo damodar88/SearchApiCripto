0658ccd26f37a8a29aeacb4006c56729fcd5e79e
damodar88 <damodarstt@gmail.com>
2025-02-03 23:21:14 -0300

    feat: Add mapper gateway to domain

---
+++ src/main/java/com/search/common/constants/ErrorMessages.java
@@ -0,0 +1,11 @@
+package com.search.common.constants;
+
+public class ErrorMessages {
+
+    public static final String CRYPTO_NOT_FOUND = "No information found for the currency: ";
+    public static final String PRICE_NOT_FOUND = "No price found for the currency: ";
+
+    private ErrorMessages() {
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    }
+}
