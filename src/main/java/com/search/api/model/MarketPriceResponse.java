1d2132b18b16ced84fbb56101a328fcb3e577854
damodar88 <damodarstt@gmail.com>
2025-02-04 23:22:11 -0300

    feat: Add builder response api

--- src/main/java/com/search/api/model/MarketPriceResponse.java
+++ src/main/java/com/search/api/model/MarketPriceResponse.java
@@ -4,7 +4,51 @@ import java.util.List;
 
 public class MarketPriceResponse {
 
-    private String cryptoId;
-    private String currency;
-    private List<PlatformPrice> prices;
+    private final String cryptoId;
+    private final String currency;
+    private final List<PlatformPrice> prices;
+
+    private MarketPriceResponse(Builder build){
+        this.cryptoId = build.cryptoId;
+        this.currency = build.currency;
+        this.prices = build.prices;
+    }
+
+    public String getCryptoId() {
+        return cryptoId;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public List<PlatformPrice> getPrices() {
+        return prices;
+    }
+
+    public static final class Builder{
+
+        private String cryptoId;
+        private String currency;
+        private List<PlatformPrice> prices;
+
+        public Builder cryptoId(String cryptoId){
+            this.cryptoId = cryptoId;
+            return this;
+        }
+
+        public Builder currency(String currency){
+            this.currency = currency;
+            return this;
+        }
+
+        public Builder prices(List<PlatformPrice> prices){
+            this.prices = prices;
+            return this;
+        }
+
+        public MarketPriceResponse build(){
+            return new MarketPriceResponse(this);
+        }
+    }
 }
