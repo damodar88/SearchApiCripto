203371787485a820584403e686dea79c76a80e08
damodar88 <damodarstt@gmail.com>
2025-02-12 01:55:29 -0300

    refactor: Add refactor functional programing in maper

--- src/main/java/com/search/api/mapper/BitcoinDtoToPriceResponseMapper.java
+++ src/main/java/com/search/api/mapper/BitcoinDtoToPriceResponseMapper.java
@@ -10,57 +10,45 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 @Component
 public class BitcoinDtoToPriceResponseMapper {
 
     public MarketPriceResponse simpleDelegateToBitcoinResponseDtoMapper(CryptoMarketPriceDomainDTO responseGeckoDto, CryptoMarketPriceDomainDTO responseBudaDto) {
-        List<PlatformPrice> geckoPrices = mapGeckoDtoToPlatformPrices(responseGeckoDto);
-        List<PlatformPrice> budaPrices = mapBudaDtoToPlatformPrices(responseBudaDto);
+
+        List<PlatformPrice> geckoPrices = requestToPlatformPriceMap(responseGeckoDto, "CoinGecko");
+        List<PlatformPrice> budaPrices = requestToPlatformPriceMap(responseBudaDto, "Buda.com");
 
         List<PlatformPrice> allPrices = new ArrayList<>();
         allPrices.addAll(geckoPrices);
         allPrices.addAll(budaPrices);
 
+        String cryptoId = Optional.ofNullable(responseGeckoDto)
+                .map(CryptoMarketPriceDomainDTO::getCryptoId)
+                .orElse(Optional.ofNullable(responseBudaDto).map(CryptoMarketPriceDomainDTO::getCryptoId).orElse("unknown"));
+
+        String currency = Optional.ofNullable(responseGeckoDto)
+                .map(CryptoMarketPriceDomainDTO::getCurrency)
+                .orElse(Optional.ofNullable(responseBudaDto).map(CryptoMarketPriceDomainDTO::getCurrency).orElse("unknown"));
+
         return new MarketPriceResponse.Builder()
-                .cryptoId(responseGeckoDto.getCryptoId())
-                .currency(responseGeckoDto.getCurrency())
+                .cryptoId(cryptoId)
+                .currency(currency)
                 .prices(allPrices)
                 .build();
     }
 
-    private List<PlatformPrice> mapGeckoDtoToPlatformPrices(CryptoMarketPriceDomainDTO responseGeckoDto) {
-        List<MarketPriceDomainDTO> marketGeckoPrices = Optional.ofNullable(responseGeckoDto.getMarketPrices())
-                .orElseThrow(() -> new IllegalArgumentException("⚠️ No se encontraron precios en CoinGecko."));
+    private List<PlatformPrice> requestToPlatformPriceMap(CryptoMarketPriceDomainDTO responseDto, String platformName) {
+        List<MarketPriceDomainDTO> marketPricesList = Optional.ofNullable(responseDto.getMarketPrices())
+                .orElseGet(ArrayList::new);
 
-        List<PlatformPrice> prices = new ArrayList<>();
-        for (MarketPriceDomainDTO marketPriceDomainDTO : marketGeckoPrices) {
-            PlatformPrice platformPrice = new PlatformPrice.Builder()
-                    .plataform(marketPriceDomainDTO.getPlatform())
-                    .price(marketPriceDomainDTO.getPrice())
-                    .timestamp(marketPriceDomainDTO.getTimestamp())
-                    .build();
-            prices.add(platformPrice);
-        }
-        return prices;
-    }
-
-    private List<PlatformPrice> mapBudaDtoToPlatformPrices(CryptoMarketPriceDomainDTO responseBudaDto) {
-        List<MarketPriceDomainDTO> marketBudaPrices = Optional.ofNullable(responseBudaDto.getMarketPrices())
-                .orElseThrow(() -> new IllegalArgumentException("⚠️ No se encontraron precios en Buda."));
-
-        List<PlatformPrice> prices = new ArrayList<>();
-        for (MarketPriceDomainDTO marketPriceDomainDTO : marketBudaPrices) {
-            PlatformPrice platformPrice = new PlatformPrice.Builder()
-                    .plataform(marketPriceDomainDTO.getPlatform())
-                    .price(marketPriceDomainDTO.getPrice())
-                    .timestamp(marketPriceDomainDTO.getTimestamp())
-                    .build();
-            prices.add(platformPrice);
-        }
-        return prices;
+        return marketPricesList.stream()
+                .map(price -> new PlatformPrice.Builder()
+                        .plataform(platformName)
+                        .price(price.getPrice())
+                        .timestamp(price.getTimestamp())
+                        .build())
+                .collect(Collectors.toList());
     }
 }
 
