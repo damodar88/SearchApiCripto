75abe8af10b86ec734a8d8a905b0cc5e0b77717f
damodar88 <damodarstt@gmail.com>
2025-02-13 20:13:40 -0300

    refactor: Adds a refactor to decouple logic from maper

--- src/main/java/com/search/domain/mapper/CoinGeckoMarketPriceMapper.java
+++ src/main/java/com/search/domain/mapper/CoinGeckoMarketPriceMapper.java
@@ -1,6 +1,5 @@
 package com.search.domain.mapper;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.search.common.exception.MapperException;
 import com.search.data.model.coingecko.CryptoPriceModel;
 import com.search.domain.modeldto.CryptoMarketPriceDomainDTO;
@@ -9,49 +8,56 @@ import com.search.domain.modeldto.MarketPriceDomainDTO;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
-import java.time.ZoneOffset;
-import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+import static com.search.common.constants.ApplicationConstants.PLATFORM_NAME;
+import static com.search.common.constants.ApplicationConstants.TIMESTAMP_FORMATTER;
 import static com.search.common.constants.ErrorMessages.CRYPTO_NOT_FOUND;
 import static com.search.common.constants.ErrorMessages.PRICE_NOT_FOUND;
 
 @Component
 public class CoinGeckoMarketPriceMapper {
 
+    public CryptoMarketPriceDomainDTO mapCoinGeckoBitcoinToDto(CoinGeckoCryptoResponse response, String cryptoCurrency,
+                                                               String selectedCurrency) {
 
-    private static final String PLATFORM_NAME = "CoinGecko";
-    private static final DateTimeFormatter TIMESTAMP_FORMATTER =
-            DateTimeFormatter.ISO_INSTANT.withZone(ZoneOffset.UTC);
-
-    public CryptoMarketPriceDomainDTO mapCoinGeckoBitcoinToDto(CoinGeckoCryptoResponse response, String cryptoCurrency, String selectedCurrency) {
-
-        CryptoPriceModel cryptoData = Optional.ofNullable(response.getCryptos().get(cryptoCurrency))
-                .orElseThrow(() -> new MapperException(CRYPTO_NOT_FOUND + cryptoCurrency));
-
-        Double price = Optional.ofNullable(cryptoData.getPrices().get(selectedCurrency))
-                .orElseThrow(() -> new MapperException(PRICE_NOT_FOUND + selectedCurrency));
+        CryptoPriceModel cryptoData = getCryptoData(response, cryptoCurrency);
+        Double price = getPrice(cryptoData, selectedCurrency);
 
         long timestampEpoch = cryptoData.getLastUpdatedAt();
-        String timestamp = timestampEpoch > 0
+
+        return new CryptoMarketPriceDomainDTO.Builder()
+                .withCryptoId(cryptoCurrency)
+                .withCurrency(selectedCurrency)
+                .withMarketPrices(getMarketPricesList(getTimeStamp(timestampEpoch), price))
+                .build();
+    }
+
+    private String getTimeStamp(long timestampEpoch){
+        return timestampEpoch > 0
                 ? TIMESTAMP_FORMATTER.format(Instant.ofEpochSecond(timestampEpoch))
                 : "N/A";
+    }
 
-        List<MarketPriceDomainDTO> marketPrices = Collections.singletonList(
+    private CryptoPriceModel getCryptoData(CoinGeckoCryptoResponse response, String cryptoCurrency) {
+        return Optional.ofNullable(response.getCryptos().get(cryptoCurrency))
+                .orElseThrow(() -> new MapperException(CRYPTO_NOT_FOUND + cryptoCurrency));
+    }
+
+    private Double getPrice(CryptoPriceModel cryptoData, String selectedCurrency) {
+        return Optional.ofNullable(cryptoData.getPrices().get(selectedCurrency))
+                .orElseThrow(() -> new MapperException(PRICE_NOT_FOUND + selectedCurrency));
+    }
+
+    private List<MarketPriceDomainDTO> getMarketPricesList(String timestamp, Double price){
+        return Collections.singletonList(
                 new MarketPriceDomainDTO.Builder()
                         .withPlatform(PLATFORM_NAME)
                         .withPrice(price)
                         .withTimestamp(timestamp)
                         .build()
         );
-
-        return new CryptoMarketPriceDomainDTO.Builder()
-                .withCryptoId(cryptoCurrency)
-                .withCurrency(selectedCurrency)
-                .withMarketPrices(marketPrices)
-                .build();
     }
-
 }
