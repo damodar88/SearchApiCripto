8b23d4304bc97058b43589152c10e6b550228ca6
damodar88 <damodarstt@gmail.com>
2025-02-14 17:43:02 -0300

    feat: Add maper buda.com to domain layer

--- src/main/java/com/search/common/constants/ErrorMessages.java
+++ src/main/java/com/search/common/constants/ErrorMessages.java
@@ -4,6 +4,7 @@ public class ErrorMessages {
 
     public static final String CRYPTO_NOT_FOUND = "No information found for the currency: ";
     public static final String PRICE_NOT_FOUND = "No price found for the currency: ";
+    public static final String PRICE_NOT_FOUND_FOR_CURRENCY = "No price found for the currency: ";
 
     private ErrorMessages() {
         throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
--- src/main/java/com/search/domain/mapper/BudaMarketPriceMapper.java
+++ src/main/java/com/search/domain/mapper/BudaMarketPriceMapper.java
@@ -1,11 +1,16 @@
 package com.search.domain.mapper;
 
+import com.search.common.exception.MapperException;
 import com.search.data.model.buda.CryptoPriceResponse;
 import com.search.domain.modeldto.CryptoMarketPriceDomainDTO;
 import com.search.domain.modeldto.MarketPriceDomainDTO;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
+import java.util.List;
+
 import static com.search.common.constants.ApplicationConstants.BUDA_MARKET_ID;
+import static com.search.common.constants.ErrorMessages.PRICE_NOT_FOUND_FOR_CURRENCY;
 
 @Component
 public class BudaMarketPriceMapper {
@@ -13,17 +18,27 @@ public class BudaMarketPriceMapper {
     public CryptoMarketPriceDomainDTO mapBudaBitcoinPriceToDto(CryptoPriceResponse cryptoPriceResponse){
 
 
-        new MarketPriceDomainDTO.Builder()
+        List<MarketPriceDomainDTO> marketPrice = Collections.singletonList(new MarketPriceDomainDTO.Builder()
                 .withPlatform(BUDA_MARKET_ID)
-                .withPrice(Double.parseDouble(cryptoPriceResponse.getTicker().getLastPrice().get(0)))
+                .withPrice(getPrice(cryptoPriceResponse))
                 .withTimestamp(cryptoPriceResponse.getTicker().getMarketId())
-                .build();
-
+                .build()
+        );
 
         return new CryptoMarketPriceDomainDTO.Builder()
                 .withCryptoId(cryptoPriceResponse.getTicker().getMarketId())
-                .withCurrency(cryptoPriceResponse.getTicker().getLastPrice())
-                .withMarketPrices(cryptoPriceResponse.getTicker())
+                .withCurrency(cryptoPriceResponse.getTicker().getMarketId())
+                .withMarketPrices(marketPrice)
                 .build();
+    }
+
+
+    private Double getPrice(CryptoPriceResponse cryptoPriceResponse){
+        if (cryptoPriceResponse.getTicker().getLastPrice().isEmpty() || cryptoPriceResponse.getTicker().getLastPrice() == null){
+            throw new MapperException(PRICE_NOT_FOUND_FOR_CURRENCY);
+        }
+
+        List<String> listPrice = Collections.singletonList(cryptoPriceResponse.getTicker().getLastPrice().get(0));
+        return Double.parseDouble(listPrice.get(0));
     }
 }
